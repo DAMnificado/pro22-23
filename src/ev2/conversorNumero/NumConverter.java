3e2d02f8f152b372d02e27e6b9c5e71f678b80b6
Héctor Aurelio Mascuñana Rodríguez <a22hectoramr@ATEIS.LOCAL>
2023-01-16 18:20:05 +0100

    conversorNumero

---
+++ src/ev2/conversorNumero/NumConverter.java
@@ -0,0 +1,22 @@
+package ev2.conversorNumero;
+
+public class NumConverter {
+    private final Integer number;
+
+    public NumConverter(Integer number) {
+        this.number = number;
+    }
+
+    public Integer getNumber() {
+        return number;
+    }
+
+    public String getNumber(char format) {
+        return switch (format) {
+            case 'B' -> Integer.toBinaryString(number);
+            case 'H' -> Integer.toHexString(number);
+            case 'O' -> Integer.toOctalString(number);
+            default -> "Error";
+        };
+    }
+}
